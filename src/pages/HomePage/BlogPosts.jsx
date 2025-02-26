d2f22ed0f5db5640e9a8aa96930fc6139c8ed06b
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-02-27 02:15:49 +0700

    Fix css

--- src/pages/HomePage/BlogPosts.jsx
+++ src/pages/HomePage/BlogPosts.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
 import news from "~/data/news.json";
 import "~/styles/blogPosts.css";
 export default function BlogPosts() {
+    const [newsList, setNewsList] = useState([]);
+    useLayoutEffect(() => {
+        setNewsList(news.reverse().slice(0, 3));
+    }, []);
     return (
         <>
             <section className="blog-posts">
@@ -18,38 +22,35 @@ export default function BlogPosts() {
 
                     <div className="blog-posts__grid">
                         {/* Blog 1 */}
-                        {news
-                            .reverse()
-                            .slice(0, 3)
-                            .map((item) => (
-                                <article className="blog-posts__item" key={item.id}>
-                                    <figure className="blog-posts__image-wrapper">
-                                        <Link to={`/blog/news/${item.slug}`}>
-                                            <img
-                                                src={`${process.env.REACT_APP_BASE_URL}${item.thumbnail}`}
-                                                alt={item.name}
-                                                className="blog-posts__image"
-                                                loading="lazy"
-                                            />
+                        {newsList.map((item) => (
+                            <article className="blog-posts__item" key={item.id}>
+                                <figure className="blog-posts__image-wrapper">
+                                    <Link to={`/blog/news/${item.slug}`}>
+                                        <img
+                                            src={`${process.env.REACT_APP_BASE_URL}${item.thumbnail}`}
+                                            alt={item.name}
+                                            className="blog-posts__image"
+                                            loading="lazy"
+                                        />
+                                    </Link>
+                                </figure>
+                                <div className="blog-posts__content">
+                                    <h3>
+                                        <Link
+                                            to={`/blog/news/${item.slug}`}
+                                            className="blog-posts__name line-clamp"
+                                            style={{ "--line-clamp": 3 }}
+                                        >
+                                            {item.name}
                                         </Link>
-                                    </figure>
-                                    <div className="blog-posts__content">
-                                        <h3>
-                                            <Link
-                                                to={`/blog/news/${item.slug}`}
-                                                className="blog-posts__name line-clamp"
-                                                style={{ "--line-clamp": 3 }}
-                                            >
-                                                {item.name}
-                                            </Link>
-                                        </h3>
-                                        <Link to={`/blog/news/${item.slug}`} className="blog-posts__link">
-                                            Read more
-                                            <ArrowRight size={20} />
-                                        </Link>
-                                    </div>
-                                </article>
-                            ))}
+                                    </h3>
+                                    <Link to={`/blog/news/${item.slug}`} className="blog-posts__link">
+                                        Read more
+                                        <ArrowRight size={20} />
+                                    </Link>
+                                </div>
+                            </article>
+                        ))}
                     </div>
                 </div>
             </section>
